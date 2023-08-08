a035d373bfdb5c63c6f7494acd06a60804823558
HoanFPTU <nguyenhoan20032003@gmail.com>
2023-08-08 22:36:19 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+const findNumber = (num) => (fun) => {
+  const result = [];
+  for (i = 1; i < num; i++) {
+    if (fun(i)) {
+      result.push(i);
+    }
+  }
+  return result;
+};
+console.log(findNumber(10)((number) => number % 2 === 1));
